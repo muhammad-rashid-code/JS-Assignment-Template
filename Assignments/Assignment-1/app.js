cec42c4389143c3482a4f60cbe09170550903c42
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-22 02:54:12 +0500

    Assignment # 1 ALERTS

--- Assignments/Assignment-1/app.js
+++ Assignments/Assignment-1/app.js
@@ -1,1 +1,7 @@
-alert("Welcome to our website! We're glad you're here.");
+//Exercise:1
+// Write a script to greet your website visitor using JS alert box.
+alert("Welcome to 'BANO QABIL' website! We're glad you're here.");
+
+//Exercise:2
+// 2. Write a script to display following message on your web page:
+// Error!Please enter a valid Password.
