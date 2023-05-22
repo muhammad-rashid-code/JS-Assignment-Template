b066bb15d76b027145ed8afe0d4068703683fbe9
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-22 16:24:54 +0500

    Assignment # 3 VARIABLES FOR NUMBERS

---
+++ DOM/app.js
@@ -0,0 +1,21 @@
+ console.log(document);
+// var myDiv = document.getElementById('myDiv');
+// console.log(myDiv);
+// var para = myDiv.getElementsByTagName('p')
+// // console.log(para)
+// console.log(para[0])
+// console.log(para[1]);
+// console.log(para.length)
+
+// console.log(document.childNodes.length);
+// console.log(document.childNodes[1]);
+// console.log(document.childNodes[1].childNodes);
+// console.log(document.childNodes[0].childNodes);
+
+// Junk article remove usama usman idra noor ehaq 14:21
+console.log(document.childNodes[1].childNodes[1].childNodes)
+for (var i=0; i<document.childNodes[1].childNodes[1].childNodes.length;i++){
+    console.log(document.childNodes[1].childNodes[1].childNodes[i].nodeType)
+}
+//16 :40
+// 5/22//2023 at 17: 40 to be continue
