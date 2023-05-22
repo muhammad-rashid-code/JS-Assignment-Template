a2c7d4180b676c0087ca7cbccc5768d11b8bda5b
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-23 02:51:07 +0500

    Assignment # 4 VARIABLE NAMES: LEGAL & ILLEGAL

---
+++ Assignments/Assignment-4/Excersice-3/app.js
@@ -0,0 +1,14 @@
+var taskA = "Rules for naming JS variables\na):Variable names can contain \nb):letters (a-z, A-Z),\nc) numbers (0-9),\n and underscores (_).\n d):They cannot contain any other special characters \nd):such as @, #, $, etc."
+alert(taskA);
+
+var taskB = 'Variable names can only contain letters (a-z, A-Z), numbers (0-9), underscores (_), and dollar signs ($).'
+alert(taskB);
+
+var taskC = 'Variables must begin with a letter (a-z, A-Z), an underscore (_), or a dollar sign ($)'
+alert(taskC);
+
+var taskD = 'Variable names are case case-sensitive '
+alert(taskD);
+
+var taskE = 'Variable names should not be JavaScript reserved keywords.'
+alert(taskE); 
