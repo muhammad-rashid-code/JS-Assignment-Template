04e4076d92690eecb743eca21f9934869729ea9a
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-23 17:21:07 +0500

    Assignment # 5 MATH EXPRESSIONS

---
+++ Assignments/Assignment-5/Excersice-4/app.js
@@ -0,0 +1,5 @@
+var ticketPrice = 100;
+var noOfticket = 5;
+var totalPrice = ticketPrice*noOfticket;
+ document.write('The total cost to buy 5 tickets to a movieis '+totalPrice );
+ alert('The total cost to buy 5 tickets to a movieis '+totalPrice);
