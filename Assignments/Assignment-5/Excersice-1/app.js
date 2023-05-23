19ed3fcdec93868ff4a8baa98732ab9a12763e00
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-23 14:25:54 +0500

    Assignment # 5 MATH EXPRESSIONS

---
+++ Assignments/Assignment-5/Excersice-1/app.js
@@ -0,0 +1,6 @@
+var num1 = +prompt('Write a first number');
+var num2 = +prompt('Write a first number');
+var sum =(num1+num2);
+
+ document.write("The sum of " + num1 + " and " + num2 + " is: " + sum);
+alert("The sum of " + num1 + " and " + num2 + " is: " + sum);
