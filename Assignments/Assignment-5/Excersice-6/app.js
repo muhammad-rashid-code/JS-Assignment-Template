c8f57c16555508a5beedc46d6c206c1c475b0e84
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-26 03:10:28 +0500

    Assignment # 5 MATH EXPRESSIONS

---
+++ Assignments/Assignment-5/Excersice-6/app.js
@@ -0,0 +1,13 @@
+
+let celsius = 28;
+
+let fahrenheit = (celsius * 9/5) + 32;
+
+console.log(`${celsius}°C is ${fahrenheit}°F`);
+
+let fahrenheit2 = 82;
+
+let celsius2 = (fahrenheit2 - 32) * 5/9;
+
+
+console.log(`${fahrenheit2}°F is ${celsius2}°C`);
